a8a9057720701ca3d79b1977555e2f646cecc713
Charlie Groves <c@sevorg.org>
2022-08-30 15:21:50 -0400

    Initial

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [ cargo clippy rust-analyzer rustc rustfmt ];
+        };
+      });
+}
