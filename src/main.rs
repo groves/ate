5be1551a6db4c571ec2aef6b1b08119dba9a207b
Charlie Groves <c@sevorg.org>
2022-09-09 07:27:36 -0400

    Test color setting

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,44 @@ struct Document {
 }
 
 impl Document {
+    fn new(mut input: Box<dyn Read>) -> Result<Document, Error> {
+        // TODO - lazily read and parse in Document::render
+        let mut buf = vec![];
+        let read = input.read_to_end(&mut buf)?;
+        let mut text = String::new();
+        let mut attrs = vec![(0, Change::AllAttributes(CellAttributes::default()))];
+        Parser::new().parse(&buf[0..read], |a| {
+            match a {
+                Print(c) => text.push(c),
+                Control(LineFeed) => text.push('\n'),
+                // TODO - hyperlinks
+                Action::CSI(CSI::Sgr(s)) => {
+                    let change = match s {
+                        Sgr::Reset => Change::AllAttributes(CellAttributes::default()),
+                        ac => Change::Attribute(match ac {
+                            Sgr::Intensity(i) => AttributeChange::Intensity(i),
+                            Sgr::Background(b) => AttributeChange::Background(b.into()),
+                            Sgr::Underline(u) => AttributeChange::Underline(u),
+                            Sgr::Blink(b) => AttributeChange::Blink(b),
+                            Sgr::Italic(i) => AttributeChange::Italic(i),
+                            Sgr::Invisible(i) => AttributeChange::Invisible(i),
+                            Sgr::StrikeThrough(s) => AttributeChange::StrikeThrough(s),
+                            Sgr::Foreground(f) => AttributeChange::Foreground(f.into()),
+                            Sgr::Inverse(_) => todo!(),
+                            Sgr::UnderlineColor(_) => todo!(),
+                            Sgr::Font(_) => todo!(),
+                            Sgr::Overline(_) => todo!(),
+                            Sgr::Reset => unreachable!(),
+                        }),
+                    };
+                    attrs.push((text.len(), change));
+                }
+                _ => (),
+            };
+        });
+        Ok(Document { text, attrs })
+    }
+
     // Takes a start byte offset into text, a number of lines to render, and the width in character
     // cells of those lines.
     // Returns the changes necessary to render those lines assuming a terminal cursor at the
@@ -38,16 +76,12 @@ impl Document {
         assert!(width > 0);
         assert!(lines > 0);
         let mut changes = vec![];
-        let mut attr_index = self.attrs.partition_point(|item| {
-            println!("Got {} to compare to {}", item.0, start);
-            item.0 < start
-        });
-        println!("got attr idx {} with len {}", attr_index, self.attrs.len());
         use unicode_segmentation::UnicodeSegmentation;
-        let mut text_idx = start;
-        let mut graphemes = self.text[text_idx..]
+        let mut graphemes = self.text[start..]
             .graphemes(true)
             .map(|g| (g, grapheme_column_width(g, None)));
+        let mut text_idx = start;
+        let mut attr_index = self.attrs.partition_point(|item| item.0 < start);
         let mut cells_in_line = 0;
         loop {
             if let Some((grapheme, cells)) = graphemes.next() {
@@ -60,7 +94,6 @@ impl Document {
                         break;
                     }
                 }
-                text_idx += grapheme.len();
                 // LATER - accumulate multiple cells into a string rather than a change per cell
                 if grapheme != "\n" {
                     while attr_index < self.attrs.len() && text_idx >= self.attrs[attr_index].0 {
@@ -70,6 +103,7 @@ impl Document {
                     changes.push(Change::Text(grapheme.to_string()));
                     cells_in_line += cells;
                 }
+                text_idx += grapheme.len();
             } else {
                 // TODO - read more out of input. Simpler thing will be if graphemes is empty.
                 // More correct thing will be if we're within a grapheme of the end to see if
@@ -84,50 +118,12 @@ impl Document {
 
 fn main() -> Result<(), Error> {
     let caps = Capabilities::new_from_env()?;
-
-    let mut underlying_term = new_terminal(caps)?;
-    underlying_term.set_raw_mode()?;
-
+    let underlying_term = new_terminal(caps)?;
     let mut term = BufferedTerminal::new(underlying_term)?;
+    term.terminal().set_raw_mode()?;
 
-    let mut stdin = stdin();
     let size = term.terminal().get_screen_size()?;
-    let mut parser = Parser::new();
-    // TODO - move reading and parsing to Document::render
-    let mut buf = vec![];
-    let read = stdin.read_to_end(&mut buf)?;
-    let mut text = String::new();
-    let mut attrs = vec![(0, Change::AllAttributes(CellAttributes::default()))];
-    parser.parse(&buf[0..read], |a| {
-        match a {
-            Print(c) => text.push(c),
-            Control(LineFeed) => text.push('\n'),
-            // TODO - hyperlinks
-            Action::CSI(CSI::Sgr(s)) => {
-                let change = match s {
-                    Sgr::Reset => Change::AllAttributes(CellAttributes::default()),
-                    ac => Change::Attribute(match ac {
-                        Sgr::Intensity(i) => AttributeChange::Intensity(i),
-                        Sgr::Background(b) => AttributeChange::Background(b.into()),
-                        Sgr::Underline(u) => AttributeChange::Underline(u),
-                        Sgr::Blink(b) => AttributeChange::Blink(b),
-                        Sgr::Italic(i) => AttributeChange::Italic(i),
-                        Sgr::Invisible(i) => AttributeChange::Invisible(i),
-                        Sgr::StrikeThrough(s) => AttributeChange::StrikeThrough(s),
-                        Sgr::Foreground(f) => AttributeChange::Foreground(f.into()),
-                        Sgr::Inverse(_) => todo!(),
-                        Sgr::UnderlineColor(_) => todo!(),
-                        Sgr::Font(_) => todo!(),
-                        Sgr::Overline(_) => todo!(),
-                        Sgr::Reset => unreachable!(),
-                    }),
-                };
-                attrs.push((text.len(), change));
-            }
-            _ => (),
-        };
-    });
-    let mut doc = Document { text, attrs };
+    let mut doc = Document::new(Box::new(stdin()))?;
     let (changes, mut last_rendered) = doc.render(0, size.rows - 2, size.cols)?;
     term.add_changes(changes);
     term.flush()?;
@@ -168,14 +164,30 @@ fn main() -> Result<(), Error> {
 #[cfg(test)]
 mod tests {
 
+    use termwiz::{color::ColorAttribute, surface::Surface};
+
     use super::*;
 
     #[test]
-    fn render_short_doc() {
-        let mut d = Document {
-            text: String::from("Hi Bye"),
-            attrs: vec![(0, Change::AllAttributes(CellAttributes::default()))],
-        };
+    fn parse_color_output() -> Result<(), Error> {
+        let input = "D\x1b[31mR\x1b[mD";
+        let mut doc = Document::new(Box::new(input.as_bytes()))?;
+        let (changes, _) = doc.render(0, 1, 10)?;
+        let mut screen = Surface::new(10, 1);
+        screen.add_changes(changes);
+        let cells = &screen.screen_cells()[0];
+        assert_eq!(ColorAttribute::Default, cells[0].attrs().foreground());
+        assert_eq!(
+            ColorAttribute::PaletteIndex(1),
+            cells[1].attrs().foreground()
+        );
+        assert_eq!(ColorAttribute::Default, cells[2].attrs().foreground());
+        Ok(())
+    }
+
+    #[test]
+    fn render_short_doc() -> Result<(), Error> {
+        let mut d = Document::new(Box::new("Hi Bye".as_bytes()))?;
         assert_eq!(
             (
                 vec![
@@ -190,8 +202,9 @@ mod tests {
                 ],
                 6
             ),
-            d.render(0, 2, 3).unwrap()
+            d.render(0, 2, 3)?
         );
-        assert_eq!((vec![], 0), d.render(6, 2, 3).unwrap());
+        assert_eq!((vec![], 0), d.render(6, 2, 3)?);
+        Ok(())
     }
 }
