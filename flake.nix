bd4b0eba0969cc50f5cbc4e46809882b73145c35
Charlie Groves <c@sevorg.org>
2022-09-22 09:57:30 -0400

    Add rr on linux

--- flake.nix
+++ flake.nix
@@ -4,14 +4,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-      in {
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [ cargo clippy rust-analyzer rustc rustfmt ];
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs;
+          [cargo clippy rust-analyzer rustc rustfmt]
+          ++ (lib.optional stdenv.isLinux rr);
+      };
+    });
 }
