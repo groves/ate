c594570039fab25287d130dd6c48a3f1044872bd
Charlie Groves <c@sevorg.org>
2022-10-04 08:28:18 -0400

    Build a nix package

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = {
     self,
     nixpkgs,
+    crane,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      craneLib = crane.lib.${system};
     in {
+      packages.default = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource ./.;
+      };
       devShell = pkgs.mkShell {
         packages = with pkgs;
           [cargo clippy gdb rust-analyzer rustc rustfmt]
