a273a0a9c0994443d4b49bccf5296d0af9fd53ad
Charlie Groves <c@sevorg.org>
2022-10-04 09:47:50 -0400

    Build on Mac

--- flake.nix
+++ flake.nix
@@ -14,14 +14,18 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       craneLib = crane.lib.${system};
+      # Mac gcc and clang don't include iconv by default, needed for building Rust bits
+      iconv = with pkgs; (lib.optional stdenv.isDarwin libiconv);
     in {
       packages.default = craneLib.buildPackage {
         src = craneLib.cleanCargoSource ./.;
+        buildInputs = iconv;
       };
       devShell = pkgs.mkShell {
         packages = with pkgs;
           [cargo clippy gdb rust-analyzer rustc rustfmt]
-          ++ (lib.optional (stdenv.isLinux && stdenv.isx86_64) rr);
+          ++ (lib.optional (stdenv.isLinux && stdenv.isx86_64) rr)
+          ++ iconv;
       };
     });
 }
